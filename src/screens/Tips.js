742d7a6c3fb13324e37137f14be2d8d8879ad42c
edem8 <eddieblack159@gmail.com>
2023-07-01 03:57:05 +0000

    candle care features

--- src/screens/Tips.js
+++ src/screens/Tips.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 function Tips() {
   return (
-    <div></div>
-  )
+    <div className="care">
+      <h2>TIPS FOR LASTING AND HEALTHY SCENTS</h2>
+
+      <ul>
+        <li>
+          The first time you burn your candle, allow the wax to melt all the way
+          to the edge
+          <p>of the vessel for the best diffusion and burning.</p>
+        </li>
+        <li>
+          Although our candles burn evenly from top to bottom, you should not
+          burn them
+          <p>for more than four hours at a time.</p>
+        </li>
+        <li>
+          When lighting a candle for the first time, burn it long enough to make
+          the top layer completely
+          <p>
+            liquid. Because of this, the candle will burn evenly
+            throughout its lifetime.
+          </p>
+        </li>
+        <li>
+          To re-center or straighten the wick, use a sturdy object. Pour the
+          liquid wax out 
+          <p>carefully if the flame is too small.</p>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Tips
+export default Tips;
