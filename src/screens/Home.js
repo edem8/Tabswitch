2c0cb396cdd7c954b62f0942ad41b69bf8fe0627
edem8 <eddieblack159@gmail.com>
2023-07-01 04:36:03 +0000

    Working on onboarding

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="HeroText">
-      <h2>HAND MANDE NATRUAL</h2>
+    <div className="onboarding">
+      <h2 id="firstchild">HAND MANDE NATRUAL</h2>
       <h2>SCENTED CANDLES</h2>
       <h2>FOR YOU SPACE</h2>
     </div>
